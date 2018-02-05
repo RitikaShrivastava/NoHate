1efe0b2c92cc942ddb5f29406607a2d7f6a949d7
RitikaShrivastava <ritika.shrivastava@utdallas.edu>
2018-02-05 15:29:10 -0600

    Update authenticate.js

--- authenticate.js
+++ authenticate.js
@@ -1,6 +1,6 @@
 var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 var natural_language_understanding = new NaturalLanguageUnderstandingV1({
-  "username": "67cb6023-5620-4d88-b465-9f2feb15fb2e",
-  "password": "Ffs8UMXuESvQ"
+  "username": "********************************************",
+  "password": "****************"
   'version_date': '2017-09-21'
 });
